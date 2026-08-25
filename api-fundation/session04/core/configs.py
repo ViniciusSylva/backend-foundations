6f3e9afa7a0416ef7f25bff915af40902c0126ac
Vinicius Sylva <viniciussilva7494@gmail.com>
2026-08-25 19:26:32 -0300

    feat(models): create database tables

--- api-fundation/session04/core/configs.py
+++ api-fundation/session04/core/configs.py
@@ -1,6 +1,9 @@
-from pydantic import BaseSettings
-from sqlalchemy.ext.declarative import declarative_base
+from typing import ClassVar
+from pydantic_settings import BaseSettings
+from sqlalchemy.orm import declarative_base
 
+# Cria a base declarativa do SQLAlchemy fora do Pydantic
+DBBaseModel = declarative_base()
 
 class Settings(BaseSettings):
     """
@@ -8,10 +11,11 @@ class Settings(BaseSettings):
     """
     API_V1_STR: str = '/api/v1'
     DB_URL: str = "postgresql+asyncpg://vini:vinicius2005@localhost:5432/faculdade"
-    DBBaseModel = declarative_base()
+    
+    DBBaseModel: ClassVar = DBBaseModel
 
     class Config: 
         case_sensitive = True
 
 
-settings = Settings()  
+settings = Settings()
