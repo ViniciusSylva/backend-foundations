05b6d99bfebdbc7b71cf9647cab387f08c3d0cea
Vinicius Sylva <viniciussilva7494@gmail.com>
2026-08-30 09:58:20 -0300

    refactor(core): adapt files from old version to new version

--- api-fundation/session05/core/database.py
+++ api-fundation/session05/core/database.py
@@ -1,12 +1,20 @@
 from sqlalchemy.orm import sessionmaker
+
 from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.ext.asyncio import AsyncEngine
 from sqlalchemy.ext.asyncio import AsyncSession
 
-
 from core.configs import settings
 
-engine: AsyncEngine = sessionmaker (
+
+engine: AsyncEngine = create_async_engine(
+    settings.DB_URL,
+    echo=True
+)
+
+
+
+SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
     expire_on_commit=False,
